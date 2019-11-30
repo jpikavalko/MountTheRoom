5ce8741b40a39e1ab73cd33a30525dbc29b044ba
Besticle <toronent@gmail.com>
2019-12-01 00:34:16 +0200

    DamageCounter.cs Toi on singletoni joka laskee kaikki otetut damaget. Pitäis toimii usealla pelaajallakin, mutta tiedähäntä.
    Player.cs
    Tän kun lisää rigidbodyhahmotyypille, niin se laskee kaikkien raajojen ottamat damaget yhteen ja lähettää DamageCounter.cs scriptille.
    
    RigidbodyDamage.cs
    Player.cs scripti lisää tän automaattiseen jokaiseen raajaan, jossa on rigidbody.

---
+++ Assets/Scripts/RigidbodyDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyDamage : MonoBehaviour
+{
+    Player _parentPlayer;
+
+    public void SetupRigidbodyDamage(Player p)
+    {
+        _parentPlayer = p;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float dmg = collision.impulse.magnitude;
+
+        if(collision.impulse.magnitude >= 100f)
+        {
+            Debug.Log(transform.name + " Damage: " + dmg);
+
+            DamageCounter.instance.TakeDamage(_parentPlayer, dmg);
+        }
+
+    }
+}
