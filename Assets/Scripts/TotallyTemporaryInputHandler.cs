eaa83caa7b37c36b3c0c1c153cf97b3d0343630c
jpikavalko <jpikavalko@gmail.com>
2019-11-30 19:25:51 +0200

    Ragdoll Character added

---
+++ Assets/Scripts/TotallyTemporaryInputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotallyTemporaryInputHandler : MonoBehaviour
+{
+    public Transform characterPart;
+    private Rigidbody characterRig;
+
+    public Transform explosionVector;
+    
+    public float explosionForce = 10f;
+
+    private void Awake()
+    {
+        characterRig = characterPart.GetComponent<Rigidbody>();    
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("h");
+            characterRig.AddForce((explosionVector.position.normalized) * explosionForce);
+        }
+    }
+}
