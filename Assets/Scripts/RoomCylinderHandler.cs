b7b6f04e8b5779406a70b91061eb10900b762765
Besticle <toronent@gmail.com>
2019-12-01 17:54:49 +0200

    - TextMeshPro lisätty - UIta lisätty tommin testisceneen

---
+++ Assets/Scripts/RoomCylinderHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCylinderHandler : MonoBehaviour
+{
+    Rigidbody _rig;
+
+    private void Awake()
+    {
+        _rig = GetComponent<Rigidbody>();
+    }
+
+    public void RoomSpinning()
+    {
+        if (_rig.isKinematic)
+        {
+            _rig.isKinematic = false;
+        }
+        else
+        {
+            _rig.isKinematic = true;
+        }
+    }
+
+    public void RoomSpinning(bool b)
+    {
+        _rig.isKinematic = b;
+    }
+}
