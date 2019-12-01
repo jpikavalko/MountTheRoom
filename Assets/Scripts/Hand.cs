3a5041d13af938d661ff03fb9b04efbb5593bceb
jpikavalko <jpikavalko@gmail.com>
2019-12-01 13:08:43 +0200

    Grabbing added

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HAND { LEFT, RIGHT }
+public class Hand : MonoBehaviour
+{
+    public LayerMask collisionLayers;
+    public HAND hand;
+
+    private GameObject itemInHand;
+    private bool isTouching = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isTouching = IsInLayerMask(other.gameObject.layer, collisionLayers);
+        if (isTouching)
+        {
+            itemInHand = other.gameObject;
+        }
+        else
+        {
+            if (itemInHand != null) itemInHand.GetComponent<Collider>().enabled = true;
+            itemInHand = null;
+        }
+    }
+
+    private static bool IsInLayerMask(int layer, LayerMask layermask)
+    {
+        return layermask == (layermask | (1 << layer));
+    }
+
+    public GameObject GetItemInHand()
+    {
+        return itemInHand != null ? itemInHand : null;   
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,44 +10,44 @@ public class PlayerController : MonoBehaviour
                      rightHand,
                      torso;
 
+    private Hand    left, right;
 
+    public float    pullForce,
+                    jumpForce;
 
-    public float pullForce,
-                 jumpForce;
+    private float   leftHorizontal,
+                    rightHorizontal,
+                    leftVertical,
+                    rightVertical;
 
-    private float leftHorizontal,
-                  rightHorizontal,
-                  leftVertical,
-                  rightVertical;
+    private bool    isJumpPressed,
+                    isLeftGrabbing,
+                    isRightGrabbing;
 
-    private bool isJumpPressed;
-
-    Camera main;
+    Camera          main;
 
 
     void Start()
     {
         main = Camera.main;
         InvokeRepeating("LousyExuceOfATimer", 1f, 0.1f);
+
+        left = leftHand.transform.GetComponentInChildren<Hand>();
+        right = rightHand.transform.GetComponentInChildren<Hand>();
     }
 
     void Update()
     {
-        leftHorizontal = Input.GetAxis("LeftHorizontal");
+        leftHorizontal  = Input.GetAxis("LeftHorizontal");
         rightHorizontal = Input.GetAxis("RightHorizontal");
-        leftVertical = Input.GetAxis("LeftVertical");
-        rightVertical = Input.GetAxis("RightVertical");
+        leftVertical    = Input.GetAxis("LeftVertical");
+        rightVertical   = Input.GetAxis("RightVertical");
 
-        isJumpPressed = Input.GetButton("Jump");
-        
-        if (isJumpPressed && a >= 20) 
-        {
-            Debug.Log("hump");
-            Jump();
-            isJumpPressed = false;
-            a = 0;
-        }
+        isJumpPressed   = Input.GetButton("Jump");
+        isLeftGrabbing  = Input.GetButton("LeftGrab");
+        isRightGrabbing = Input.GetButton("RightGrab");
     }
+
     int a = 15;
     private void LousyExuceOfATimer()
     {
@@ -56,30 +56,55 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //MoveHand(leftHand, new Vector3(leftHorizontal, -leftVertical, 0));
-        //MoveHand(rightHand, new Vector3(leftHorizontal, -leftVertical, 0));
         MoveHand(rightHand, new Vector3(rightHorizontal, -rightVertical, 0));
         MoveHand(leftHand, new Vector3(rightHorizontal, -rightVertical, 0));
         MoveTorso();
 
-        Debug.DrawLine(target.position, torso.position, Color.green);
+        if (isJumpPressed && a >= 20)
+        {
+            Jump();
+            isJumpPressed = false;
+            a = 0;
+        }
+
+        if (isLeftGrabbing) GrabObject(left);
+        if (isRightGrabbing) GrabObject(right);   
+    }
+
+    private void GrabObject(Hand hand)
+    {
+        GameObject carryItem = hand.GetItemInHand();
+
+        if (carryItem != null)
+        {
+            carryItem.GetComponent<Collider>().enabled = false;
+            carryItem.transform.position = hand.transform.position;
+            carryItem.transform.rotation = hand.transform.rotation;
+        }
     }
 
     private void MoveHand(Rigidbody hand, Vector3 direction)
     {
+        // Debug shows the vector of hands
+        Debug.DrawRay(hand.position, direction * 4f, Color.magenta);
+
         direction = main.transform.TransformDirection(direction);
         hand.AddForce(direction * pullForce);
     }
 
     private void MoveTorso()
     {
+        // Debug shows direction of vector (not the same as torso.addforce thingy)
+        Debug.DrawRay(torso.position, new Vector3(leftHorizontal, -leftVertical, 0f) * 4f, Color.red);
+        Debug.DrawRay(torso.position, new Vector3(leftHorizontal, -leftVertical, 0f), Color.green);
+
         torso.AddForce(-Vector3.forward * leftVertical * 300f);
         torso.AddForce(main.transform.TransformDirection(new Vector3(leftHorizontal, 0f, 0f) * 50f));
     }
 
     private void Jump()
     {
-        Vector3 direction = (target.position - torso.position);
+        Vector3 direction = (target.position - torso.position).normalized;
         torso.AddForce(direction * jumpForce);
         
     }
